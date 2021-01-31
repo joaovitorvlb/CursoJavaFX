1a26d135ff3e556a7a846f6c509bdea2cc591991
joaovitorvlb <joaovitorvlb@hotmail.com>
2021-01-31 19:45:41 -0300

    Adicionado botao

--- CursoJavaFX/src/main/java/exemplo1/Exemplo1.java
+++ CursoJavaFX/src/main/java/exemplo1/Exemplo1.java
@@ -2,6 +2,7 @@ package exemplo1;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -15,24 +16,24 @@ public class Exemplo1 extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		// Layout
-		FlowPane noRaiz = new FlowPane();
-
-		// Cena
-		Scene minhaCena = new Scene(noRaiz, 300, 200);
-
-		// Nó
-		Label label = new Label();
-		label.setText("Olá mundo!");
-
-		// Adicionar elementos em cena
-		noRaiz.getChildren().add(label);
-
-		// Adicionar a cena ao palco
-		primaryStage.setScene(minhaCena);
-
-		// Exibir o palco e sua(s) cena(s)
-		primaryStage.show();
+		//Layout
+        FlowPane noRaiz = new FlowPane(); 
+        
+        //Cena
+        Scene minhaCena = new Scene(noRaiz, 300, 200);
+        
+        //Nó
+        Button botao = new Button();
+        botao.setText("Olá mundo!");
+        
+        //Adicionar elementos em cena
+        noRaiz.getChildren().add(botao);
+        
+        //Adicionar a cena ao palco
+        primaryStage.setScene(minhaCena);
+        
+        //Exibir o palco e sua(s) cena(s)
+        primaryStage.show();
 	}
 
 	public static void main(String args[]) {
